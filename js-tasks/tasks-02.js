982c3384398f3396c3eeaf6851a112aaa8813197
PetrashchukIM <39243711+PetrashchukIM@users.noreply.github.com>
2024-05-21 12:13:36 +0000

    Task 2 js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,12 +1,15 @@
-https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-v1 - Ivan
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+//v1 - Ivan
 function stringToArray(string){
 	return string.split(' ');
 }
 
-https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-v1 - Ivan
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+//v1 - Ivan
 function DNAtoRNA(dna) {
   return dna.replace(/T/g, 'U');
 }
 
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+//here is some mistake
