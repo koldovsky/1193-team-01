0f21f98e97543ae3c0bc7c8000840bc5334fe995
PetrashchukIM <39243711+PetrashchukIM@users.noreply.github.com>
2024-05-21 07:58:03 +0000

    add task2 js

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+v1 - Ivan
+function stringToArray(string){
+	return string.split(' ');
+}
+
+https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+v1 - Ivan
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+
