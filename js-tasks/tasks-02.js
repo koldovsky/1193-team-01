5bc95aaf5fcb6432515b673353290e3b0ed281b6
PetrashchukIM <39243711+PetrashchukIM@users.noreply.github.com>
2024-05-22 09:53:10 +0000

    change task 2

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,17 +1,14 @@
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-//v1 - Ivan
 function stringToArray(string) {
   return string.split(" ");
 }
 
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-//v1 - Ivan
 function DNAtoRNA(dna) {
   return dna.replace(/T/g, "U");
 }
 
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-//v1 - Ivan
 function min(arr) {
   return Math.min(...arr);
 }
@@ -21,7 +18,6 @@ function max(arr) {
 }
 
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
-//v1 - Ivan
 function min(arr, returnType) {
   let minValue = Math.min(...arr);
   if (returnType == "value") {
