84d5df1ac5e903373bba9da1ffd4d131f091a2fa
holovanych <168016056+holovanych@users.noreply.github.com>
2024-05-24 14:01:06 +0000

    Additional task

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -19,3 +19,15 @@ function giveMeFive(obj) {
   }
   return result;
 }
+
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+function buildFun(n) {
+  var res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      console.log(i);
+      return i;
+    });
+  }
+  return res;
+}
