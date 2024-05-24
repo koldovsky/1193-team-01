925b41a56f8686ea64c3d24c97ebbd147ec09132
holovanych <168016056+holovanych@users.noreply.github.com>
2024-05-24 13:56:27 +0000

    Ad tasks 3

---
+++ js-tasks/tasks-03.js
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  return 2 * Math.PI * circle.radius;
+}
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  const result = [];
+  for (let key in obj) {
+    if (key.length === 5) result.push(key);
+    if (obj[key].length === 5) result.push(obj[key]);
+  }
+  return result;
+}
