9c528949634ce021d2f23f2fda9bc99e88f5bcf8
Taras Holovanych <tarasgolovanush@gmail.com>
2024-05-30 23:05:44 +0300

    Fix errors

--- js/product-main.js
+++ js/product-main.js
@@ -2,7 +2,7 @@ import { ProductsService } from './products-service.js';
 
 const service = new ProductsService();
 const products = await service.getProducts();
-const id = localStorage.getItem("product-page-id");
+const id = parseInt(localStorage.getItem("product-page-id"));
 
 const findedProduct = products.find((item)=>{
     return item.id === id;
