ebcae3904bdb7467cc3ea707fdf4b47dcbd01ef0
YaroslavZag <150823786+YaroslavZag@users.noreply.github.com>
2024-05-30 18:47:39 +0000

    update code

--- js/product.js
+++ js/product.js
@@ -1,14 +1,10 @@
 (function() {
    
   function showInfo() {
-    const product = JSON.parse(localStorage.product || "{}");
-    if (!product) return;
-    document.querySelector('.main__product-title')
-      .innerText = product.title;
-    document.querySelector('.main__product-info')
-      .innerText = product.code;
-    document.querySelector('.main__product-price')
-      .innerText = product.price;
+    const storedProduct = localStorage.getItem('product');
+    if (!storedProduct) return;
+    const product = JSON.parse(storedProduct);
+    document.querySelector('.main__product-title').innerText = product.title;
   }
 
   showInfo();
