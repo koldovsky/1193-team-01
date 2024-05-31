21703dac38ec4b28e55ec5b3000e5a4828102434
YaroslavZag <150823786+YaroslavZag@users.noreply.github.com>
2024-05-31 08:04:27 +0000

    Update js code in my page. Page genegeded info. Delete product-list.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 function init() {
     import('./header-burger-menu.js');    
     import('./product-tabs.js');
-    import('./product-list.js');
     import('./headphones-carousel.js');
     import('./customer-review.js');
     import('./products-service.js');
--- js/product-main.js
+++ js/product-main.js
@@ -1,13 +1,34 @@
-import { ProductsService } from './products-service.js';
+import { ProductsService } from "./products-service.js";
 
 const service = new ProductsService();
 const products = await service.getProducts();
 const id = parseInt(localStorage.getItem("product-page-id"));
 
-const findedProduct = products.find((item)=>{
-    return item.id === id;
-})
+const findedProduct = products.find((item) => {
+  return item.id === id;
+});
 
-console.log(products);
+const imageBigProduct = document.querySelector(".main__product-container");
+const imageSmallProduct = document.querySelector(".main__product-img");
+const titleProduct = document.querySelector(".main__product-title");
+const codeProduct = document.querySelector(".main__product-info");
+const priceProduct = document.querySelector(".main__product-price");
+const decscriptionProduct = document.querySelector(".main__product-text");
 
+titleProduct.innerText = findedProduct.name;
+codeProduct.innerText += findedProduct.code;
+priceProduct.innerText += findedProduct.price;
+decscriptionProduct.innerHTML = findedProduct.description;
+imageBigProduct.innerHTML = `<img
+                    src="${findedProduct.image}"
+                    alt="${findedProduct.name}"
+                    class="main__product-image"
+                    />`;
+imageSmallProduct.innerHTML = `<img
+                    width="104"
+                    src="${findedProduct.image}"
+                    alt="${findedProduct.name}"
+                    class="main__product-images"
+                    />`;
 console.log(findedProduct);
+console.log(findedProduct.name);
